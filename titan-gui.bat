9b584d74b37cf963e42a202444eac994b9aaba45
Ryzen <kbayu788@gmail.com>
2024-07-08 17:56:46 +0700

    titan-gui.bat

--- titan-gui.bat
+++ titan-gui.bat
@@ -34,31 +34,39 @@ rmdir /s /q "C:\Program Files (x86)\titan_network" 2>nul
 echo [1;33mUninstall and clean completed.[0m
 pause
 
-:: Step 2: Download master file and extract to c:\titan-master
+:: Step 2: Download master file and extract to %TEMP%\titan-master
 :download_extract_files
 echo [1;33mRunning download and extraction process for supporting files...[0m
 
 rem Downloading titan-master.zip with progress indication
 echo Downloading titan-master.zip...
-powershell -command "& {Invoke-WebRequest 'https://ryzen.sgp1.cdn.digitaloceanspaces.com/titan-edge.zip' -OutFile '%USERPROFILE%\Desktop\titan-master.zip' -UseBasicParsing}"
+set DOWNLOAD_PATH=%TEMP%\titan-master.zip
+powershell -command "& {Invoke-WebRequest 'https://ryzen.sgp1.cdn.digitaloceanspaces.com/titan-edge.zip' -OutFile '%DOWNLOAD_PATH%' -UseBasicParsing}"
 if %errorlevel% neq 0 (
     echo [1;31mFailed to download titan-master.zip. Exiting installation.[0m
     pause
     exit /b
 ) else (
-    echo [1;32mDownload successful. File saved to: %USERPROFILE%\Desktop\titan-master.zip[0m
+    echo [1;32mDownload successful. File saved to: %DOWNLOAD_PATH%[0m
 )
 
-rem Extracting files to c:\titan-master
-echo Extracting files to c:\titan-master...
-powershell -command "Expand-Archive -Path '%USERPROFILE%\Desktop\titan-master.zip' -DestinationPath 'c:\titan-master' -Force"
-if %errorlevel% neq 0 (
-    echo [1;31mFailed to extract files. Exiting installation.[0m
+rem Check if the file exists before extracting
+if exist "%DOWNLOAD_PATH%" (
+    rem Extracting files to %TEMP%\titan-master
+    echo Extracting files to %TEMP%\titan-master...
+    powershell -command "Expand-Archive -Path '%DOWNLOAD_PATH%' -DestinationPath '%TEMP%\titan-master' -Force"
+    if %errorlevel% neq 0 (
+        echo [1;31mFailed to extract files. Exiting installation.[0m
+        pause
+        exit /b
+    ) else (
+        echo [1;32mExtraction successful. Files extracted to: %TEMP%\titan-master[0m
+        del "%DOWNLOAD_PATH%" 2>nul
+    )
+) else (
+    echo [1;31mFile titan-master.zip not found. Exiting installation.[0m
     pause
     exit /b
-) else (
-    echo [1;32mExtraction successful. Files extracted to: c:\titan-master[0m
-    del "%USERPROFILE%\Desktop\titan-master.zip" 2>nul
 )
 
 echo [1;33mSupporting files installation completed successfully.[0m
@@ -69,7 +77,7 @@ pause
 echo [1;33mInstalling certificates and vcredist...[0m
 
 rem Download and install the root certificate
-certutil -addstore "Root" "c:\titan-master\isrgrootx1.der"
+certutil -addstore "Root" "%TEMP%\titan-master\isrgrootx1.der"
 if %errorlevel% neq 0 (
     echo [1;31mFailed to install the root certificate.[0m
     pause
@@ -79,7 +87,7 @@ if %errorlevel% neq 0 (
 )
 
 rem Install VC_redist
-start /wait "" "c:\titan-master\VC_redist.x64.exe" /install /quiet /norestart
+start /wait "" "%TEMP%\titan-master\VC_redist.x64.exe" /install /quiet /norestart
 if %errorlevel% neq 0 (
     echo [1;31mFailed to install VC_redist.[0m
     pause
@@ -93,7 +101,7 @@ pause
 :: Step 4: Install Titan Windows application
 :install_titan_exe
 echo [1;33mInstalling Titan Windows application...[0m
-start /wait "" "c:\titan-master\titan_network_windows_v0.0.10.exe" /install /quiet /norestart
+start /wait "" "%TEMP%\titan-master\titan_network_windows_v0.0.10.exe" /install /quiet /norestart
 if %errorlevel% neq 0 (
     echo [1;31mFailed to install Titan Windows application.[0m
     pause
